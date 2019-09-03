09b900ff9038e152de69bee52356a4182a8bbc69
Toni-Ty <antonique2479@yahoo.com>
2019-09-03 03:30:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,18 +1,18 @@
-class Application
-
-  def call(env)
-    resp = Rack::Response.new
-
-    time = Time.now
-
-    if
-      time.hour < 12
-      resp.write "Good Morning!"
-    elsif
-      time.hour > 12
-      resp.write "Good Afternoon!"
-    end
-  end
+# class Application
+#
+#   def call(env)
+#     resp = Rack::Response.new
+#
+#     time = Time.now
+#
+#     if
+#       time.hour < 12
+#       resp.write "Good Morning!"
+#     elsif
+#       time.hour > 12
+#       resp.write "Good Afternoon!"
+#     end
+#   end
 
 
 
