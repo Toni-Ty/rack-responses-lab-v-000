5e750b79db66090c3c8f8363736aa36187bbee4e
Toni-Ty <antonique2479@yahoo.com>
2019-09-03 03:30:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,3 +13,22 @@ class Application
       resp.write "Good Afternoon!"
     end
   end
+
+
+  
+  class Application
+  
+     def call(env)
+      resp = Rack::Response.new
+  
+       if Time.now.hour.between?(0, 11)
+        resp.write "Good Morning!"
+      elsif Time.now.hour.between?(12, 17)
+        resp.write "Good Afternoon."
+      else
+        resp.write "Good Evening!"
+      end
+  
+       resp.finish
+    end
+  end
